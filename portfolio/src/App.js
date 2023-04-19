24f55a8d73c09bd1acd571c2c7738779cb4cc866
ttyyree <cjsrnr90218@gmail.com>
2023-04-19 23:35:54 +0900

    portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,46 @@
 import './App.css';
+import MainText from './components/mainText.js';
 
 function App() {
   var docStyle = document.documentElement.style;
-
-// setting CSS variables inside this JS where they 
-// can be declaratively used inside the CSS file.
-document.addEventListener('mousemove', function(e) {
-  docStyle.setProperty('--mouse-x', e.clientX);
-  docStyle.setProperty('--mouse-y', e.clientY);
-});
+  document.addEventListener('mousemove', function (e) {
+    docStyle.setProperty('--mouse-x', e.clientX);
+    docStyle.setProperty('--mouse-y', e.clientY);
+  });
   return (
     <div className="App">
       <Nav></Nav>
-      <div className="ball"></div>
+      <MainText />
+      <div className='eyes'>
+        <div className='eye'>
+          <div className="whiteEyes">
+        <div className="ball"></div>
+      </div>
+      <div className="whiteEyes">
+        <div className="ball"></div>
+      </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
 
-function Nav(){
-  <>
-    <div>logo</div>
-    <div>
-      <div>about</div>
-      <div>project</div>
-      <div>contact</div>
+function Nav() {
+  return (
+    <div className='nav'>
+      <div className="navbar">
+      <div className='logo'>Seungyeon's space</div>
+      <div className="navbarR">
+        <div>about</div>
+        <div>project</div>
+        <div>contact</div>
+      </div>
     </div>
-  </>
+    </div>
+    
+  )
 }
 
 export default App;
---
+++ portfolio/src/components/mainText.js
@@ -0,0 +1,43 @@
+function MainText (){
+
+
+    return (
+        <div className="main-text">
+            <div>
+                <span className="letter">C</span>
+                <span className="letter">R</span>
+                <span className="letter">E</span>
+                <span className="letter">A</span>
+                <span className="letter">T</span>
+                <span className="letter">I</span>
+                <span className="letter">V</span>
+                <span className="letter">E</span>
+            </div>
+            <div>
+                <span className="letter">D</span>    
+                <span className="letter">E</span>    
+                <span className="letter">V</span>    
+                <span className="letter">E</span>    
+                <span className="letter">L</span>    
+                <span className="letter">O</span>    
+                <span className="letter">P</span>    
+                <span className="letter">E</span>    
+                <span className="letter">R</span>    
+            </div>
+            <div>
+                <span className="letter">&</span>
+                <span className="letter">P</span>
+                <span className="letter">U</span>
+                <span className="letter">B</span>
+                <span className="letter">L</span>
+                <span className="letter">I</span>
+                <span className="letter">S</span>
+                <span className="letter">H</span>
+                <span className="letter">E</span>
+                <span className="letter">R</span>
+            </div>
+        </div>
+    )
+}
+
+export default MainText;
