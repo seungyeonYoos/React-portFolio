cb5c83a6d5e355ce290b06e7f514b082ed6ffd69
ttyyree <cjsrnr90218@gmail.com>
2023-04-25 00:11:39 +0900

    mainText

--- portfolio/src/components/mainText.js
+++ portfolio/src/components/mainText.js
@@ -1,53 +1,98 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import $ from 'jquery';
 
 function MainText (){
-    let [textCount, setTextCount] = useState(0);
-    useEffect(()=>{
-        // document.getElementById("letter").addEventListener('mouseover',() =>{
-        //     setTextCount( textCount + 1 );
-        //     console.log(textCount)
-        // })
-        document.getElementsByClassName('letter')[0].addEventListener('mouseover',() =>{
-            setTextCount( textCount + 1 );
-            console.log(textCount)
-        })
-    })
+    // let [textCount, setTextCount] = useState(0);
+    // let textColor;
+    
+    // if ( textCount == 1 ){
+    //     textColor= '#D0B069';
+    // } else if ( textCount == 2 ){
+    //     textColor= '#DEDDD0';
+    // } else {
+    //     textColor = '#3E3B37';
+    // }
+    // useEffect(()=>{
+    // //     const timeout = setTimeout(() => {
+    // //   setTextColor('brown');
+    // // }, 2000);
+
+    //     $('.letter1').mouseover(function(){
+    //         setTextCount( textCount + 1 );
+    //         console.log(textCount);
+    //         if(textCount == 3){
+    //             setTextCount (0)
+    //         };
+    //     })
+    // })
+    const [counts, setCounts] = useState(new Array(25).fill(0));
+    const [textColor, setTextColor] = useState('brown');
+  
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        setTextColor('brown');
+      }, 2000);
+  
+      return () => clearTimeout(timeout);
+    }, [textColor]);
+  
+    const letters = ['C', 'R', 'E', 'A', 'T', 'I', 'V', 'E', 'D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R', '&', 'P', 'U', 'B', 'L', 'I', 'S', 'H', 'E', 'R'];
+  
+    const handleMouseOver = (index) => {
+      const newCounts = [...counts];
+      newCounts[index] = (newCounts[index] + 1) % 4;
+      setCounts(newCounts);
+    };
+  
+    const getTextColor = (count) => {
+      switch (count) {
+        case 0:
+          return '#3E3B37';
+        case 1:
+          return '#D0B069';
+        case 2:
+          return '#DEDDD0';
+        case 3:
+          return '#3E3B37';
+        default:
+          return '#3E3B37';
+      }
+    };
 
     return (
         <div className="main-text">
             <div className="main-1">
-                <span className="letter" id="letter">C</span>
-                <span className="letter">R</span>
-                <span className="letter">E</span>
-                <span className="letter">A</span>
-                <span className="letter">T</span>
-                <span className="letter">I</span>
-                <span className="letter">V</span>
-                <span className="letter">E</span>
+            {letters.slice(0, 8).map((letter, index) => (
+            <span
+              key={index}
+              style={{ color: getTextColor(counts[index]) }}
+              onMouseOver={() => handleMouseOver(index)}
+            >
+              {letter}
+            </span>
+          ))}
             </div>
             <div className="main-2">
-                <span className="letter">D</span>    
-                <span className="letter">E</span>    
-                <span className="letter">V</span>    
-                <span className="letter">E</span>    
-                <span className="letter">L</span>    
-                <span className="letter">O</span>    
-                <span className="letter">P</span>    
-                <span className="letter">E</span>    
-                <span className="letter">R</span>    
+            {letters.slice(8, 17).map((letter, index) => (
+            <span
+              key={index + 7}
+              style={{ color: getTextColor(counts[index + 7]) }}
+              onMouseOver={() => handleMouseOver(index + 7)}
+            >
+              {letter}
+            </span>
+          ))}  
             </div>
             <div className="main-3">
-                <span className="letter">&</span>
-                <span className="letter">P</span>
-                <span className="letter">U</span>
-                <span className="letter">B</span>
-                <span className="letter">L</span>
-                <span className="letter">I</span>
-                <span className="letter">S</span>
-                <span className="letter">H</span>
-                <span className="letter">E</span>
-                <span className="letter">R</span>
+            {letters.slice(17).map((letter, index) => (
+            <span
+              key={index + 17}
+              style={{ color: getTextColor(counts[index + 14]) }}
+              onMouseOver={() => handleMouseOver(index + 14)}
+            >
+              {letter}
+            </span>
+          ))}
             </div>
         </div>
     )
