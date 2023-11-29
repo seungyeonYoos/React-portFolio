ccc758f81bd7c3becfa399a287a74097112de58f
yeon <yeon@socialerus.com>
2023-11-29 17:31:09 +0900

    feat:수정

--- portfolio/src/components/skill.js
+++ portfolio/src/components/skill.js
@@ -14,69 +14,69 @@ function Skill() {
     //     // }
     // });
 
-    const [isScrolled, setIsScrolled] = useState(false);
-    const [isTools, setIsTools] = useState(false);
-    const [isDesignTools, setIsDesignTools] = useState(false);
+    // const [isScrolled, setIsScrolled] = useState(false);
+    // const [isTools, setIsTools] = useState(false);
+    // const [isDesignTools, setIsDesignTools] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //         const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-            // 스크롤 위치가 1500 이상인 경우
-            if (scrollPosition >= 1400) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
-            // 스크롤 위치가 1700 이상인 경우
-            if (scrollPosition >= 1700) {
-                setIsTools(true);
-            } else {
-                setIsTools(false);
-            }
-        };
+    //         // 스크롤 위치가 1500 이상인 경우
+    //         if (scrollPosition >= 1400) {
+    //             setIsScrolled(true);
+    //         } else {
+    //             setIsScrolled(false);
+    //         }
+    //         // 스크롤 위치가 1700 이상인 경우
+    //         if (scrollPosition >= 1700) {
+    //             setIsTools(true);
+    //         } else {
+    //             setIsTools(false);
+    //         }
+    //     };
 
-        window.addEventListener('scroll', handleScroll);
+    //     window.addEventListener('scroll', handleScroll);
 
-        // clean-up function
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+    //     // clean-up function
+    //     return () => window.removeEventListener('scroll', handleScroll);
+    // }, []);
 
     return (
         <div className="skillComponent">
             <div className="skillComponent-1">
-                <div className={`text${isScrolled ? ' scrolled' : ''}`}>
+                <div data-aos="zoom-in">
                     <p>SKILLS&</p>
                     <p>USING TOOLS</p>
                 </div>
             </div>
             <div className="skillComponent-2">
                 <div className='toolsBox'>
-                    <div className={`text${isTools ? ' scrolled' : ''}`}>
+                    <div>
                         <p className="tools">TOOLS</p>
                         <p className="tools-text">As a UI/UX Designer, I am efficient in both designing websites from<br />scratch, or helping a brand redesign.</p>
                     </div>
                     <div>
-                        <p>HTML</p>
-                        <p id='css'>CSS</p>
-                        <p id='scss'>SCSS</p>
-                        <p id='java'>JAVASCRIPT</p>
-                        <p id='jquery'>JQUERY</p>
-                        <p id='git'>GITHUB</p>
-                        <p id='react'>REACT JS</p>
-                        <p id='node'>NODE JS</p>
+                        <p data-aos="zoom-out-right">HTML</p>
+                        <p id='css' data-aos="zoom-out-right">CSS</p>
+                        <p id='scss' data-aos="zoom-out-right">SCSS</p>
+                        <p id='java' data-aos="zoom-out-right">JAVASCRIPT</p>
+                        <p id='jquery' data-aos="zoom-out-right">JQUERY</p>
+                        <p id='git' data-aos="zoom-out-right">GITHUB</p>
+                        <p id='react' data-aos="zoom-out-right">REACT JS</p>
+                        <p id='node' data-aos="zoom-out-right">NODE JS</p>
                     </div>
                 </div>
                 <div className='designToolsBox'>
-                    <div className={`text${isTools ? ' scrolled' : ''}`}>
+                    <div>
                         <p className="tools">DESIGN TOOLS</p>
                         <p className="tools-text">As a UI/UX Designer, I am efficient in both designing websites from<br />scratch, or helping a brand redesign.</p>
                     </div>
                     <div>
                         <hr className='hr'></hr>
-                        <p id='photo'>PHOTOSHOP</p>
-                        <p id='illustration'>ILLUSTRATOR</p>
-                        <p id='figma'>FIGMA</p>
+                        <p id='photo' data-aos="zoom-out-right">PHOTOSHOP</p>
+                        <p id='illustration' data-aos="zoom-out-right">ILLUSTRATOR</p>
+                        <p id='figma' data-aos="zoom-out-right">FIGMA</p>
                     </div>
                 </div>
             </div>
